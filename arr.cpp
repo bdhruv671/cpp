e3328257494978e729cd6fb233c61dbe55ae3833
dhruv bisht <118956848+bdhruv671@users.noreply.github.com>
2024-06-10 09:48:38 +0000

    basic of array

---
+++ arr.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // this is 1d arr
+    int arr[5];
+    // cin >> arr[1]>>arr[2];
+    // cout << arr[1]<<endl;
+
+// this is 2d array 
+    int b[2][3];
+    b[0][1]= 45;
+    cout << b[0][1]<< endl ;
+    return 0;
+}
